6064f3a4ad4494fcbb37e0c19dc823620e90042d
caleb <caleb>
2015-12-09 15:24:46 -0500

    Solutions to 4

---
+++ 4/b.py
@@ -0,0 +1,16 @@
+import hashlib
+
+secret = "iwrupvqb"
+counter = 0
+
+
+def my_hash(seed):
+    m = hashlib.md5()
+    m.update(seed)
+    return m.hexdigest()
+
+
+while my_hash(secret + str(counter)).startswith('000000') is False:
+    counter += 1
+
+print counter
